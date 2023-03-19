a48c6b162505c508d8927c63a92ae22b565c398f
PabloB2G <pablobermudezgamez@uma.es>
2023-03-19 13:05:03 +0100

    Inserciones menos articulo_autores

--- creacion_base.sql
+++ creacion_base.sql
@@ -5,6 +5,9 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
 -- Schema TrabajoBDB
 -- -----------------------------------------------------
 DROP SCHEMA IF EXISTS `TrabajoBDB` ;
@@ -12,148 +15,60 @@ DROP SCHEMA IF EXISTS `TrabajoBDB` ;
 -- -----------------------------------------------------
 -- Schema TrabajoBDB
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `TrabajoBDB` DEFAULT CHARACTER SET utf8 ;
+CREATE SCHEMA IF NOT EXISTS `TrabajoBDB` DEFAULT CHARACTER SET utf8mb3 ;
 USE `TrabajoBDB` ;
 
 -- -----------------------------------------------------
 -- Table `TrabajoBDB`.`Enfermedad`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `TrabajoBDB`.`Enfermedad` ;
+
 CREATE TABLE IF NOT EXISTS `TrabajoBDB`.`Enfermedad` (
-  `idEnfermedad` VARCHAR(45) NOT NULL,
-  `Pfrevalencia` VARCHAR(45) NULL,
-  `CambioProteina` VARCHAR(45) NULL,
+  `idEnfermedad` VARCHAR(100) NOT NULL,
+  `CambioProteina` VARCHAR(45) NULL DEFAULT NULL,
+  `SignificanciaClinica` VARCHAR(100) NULL DEFAULT NULL,
   PRIMARY KEY (`idEnfermedad`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `TrabajoBDB`.`Proteina`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `TrabajoBDB`.`Proteina` (
-  `idProteina` VARCHAR(45) NOT NULL,
-  `Longitud` INT NULL,
-  `Nombre` VARCHAR(45) NULL,
-  `Organismo` VARCHAR(45) NULL,
-  PRIMARY KEY (`idProteina`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `TrabajoBDB`.`Proteina_has_Enfermedad`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `TrabajoBDB`.`Proteina_has_Enfermedad` (
-  `Proteina_idProteina` VARCHAR(45) NOT NULL,
-  `Enfermedad_idEnfermedad` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`Proteina_idProteina`, `Enfermedad_idEnfermedad`),
-  INDEX `fk_Proteina_has_Enfermedad_Enfermedad1_idx` (`Enfermedad_idEnfermedad` ASC) VISIBLE,
-  INDEX `fk_Proteina_has_Enfermedad_Proteina_idx` (`Proteina_idProteina` ASC) VISIBLE,
-  CONSTRAINT `fk_Proteina_has_Enfermedad_Proteina`
-    FOREIGN KEY (`Proteina_idProteina`)
-    REFERENCES `TrabajoBDB`.`Proteina` (`idProteina`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Proteina_has_Enfermedad_Enfermedad1`
-    FOREIGN KEY (`Enfermedad_idEnfermedad`)
-    REFERENCES `TrabajoBDB`.`Enfermedad` (`idEnfermedad`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `TrabajoBDB`.`Sintomas`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `TrabajoBDB`.`Sintomas` (
-  `idSintomas` VARCHAR(45) NOT NULL,
-  `Descripcion` VARCHAR(45) NULL,
-  PRIMARY KEY (`idSintomas`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `TrabajoBDB`.`Sintomas_has_Enfermedad`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `TrabajoBDB`.`Sintomas_has_Enfermedad` (
-  `Sintomas_idSintomas` VARCHAR(45) NOT NULL,
-  `Enfermedad_idEnfermedad` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`Sintomas_idSintomas`, `Enfermedad_idEnfermedad`),
-  INDEX `fk_Sintomas_has_Enfermedad_Enfermedad1_idx` (`Enfermedad_idEnfermedad` ASC) VISIBLE,
-  INDEX `fk_Sintomas_has_Enfermedad_Sintomas1_idx` (`Sintomas_idSintomas` ASC) VISIBLE,
-  CONSTRAINT `fk_Sintomas_has_Enfermedad_Sintomas1`
-    FOREIGN KEY (`Sintomas_idSintomas`)
-    REFERENCES `TrabajoBDB`.`Sintomas` (`idSintomas`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Sintomas_has_Enfermedad_Enfermedad1`
-    FOREIGN KEY (`Enfermedad_idEnfermedad`)
-    REFERENCES `TrabajoBDB`.`Enfermedad` (`idEnfermedad`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
 -- Table `TrabajoBDB`.`Articulo`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `TrabajoBDB`.`Articulo` ;
+
 CREATE TABLE IF NOT EXISTS `TrabajoBDB`.`Articulo` (
   `idArticulo` VARCHAR(45) NOT NULL,
-  `Nombre` VARCHAR(45) NULL,
-  `Fecha` DATE NULL,
-  `Enfermedad_idEnfermedad` VARCHAR(45) NOT NULL,
+  `Nombre` VARCHAR(45) NULL DEFAULT NULL,
+  `Fecha` DATE NULL DEFAULT NULL,
+  `Enfermedad_idEnfermedad` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`idArticulo`),
   INDEX `fk_Articulo_Enfermedad1_idx` (`Enfermedad_idEnfermedad` ASC) VISIBLE,
   CONSTRAINT `fk_Articulo_Enfermedad1`
     FOREIGN KEY (`Enfermedad_idEnfermedad`)
-    REFERENCES `TrabajoBDB`.`Enfermedad` (`idEnfermedad`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `TrabajoBDB`.`Genes`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `TrabajoBDB`.`Genes` (
-  `Nombre` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`Nombre`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `TrabajoBDB`.`Enfermedad_has_Genes`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `TrabajoBDB`.`Enfermedad_has_Genes` (
-  `Enfermedad_idEnfermedad` VARCHAR(45) NOT NULL,
-  `Genes_Nombre` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`Enfermedad_idEnfermedad`, `Genes_Nombre`),
-  INDEX `fk_Enfermedad_has_Genes_Genes1_idx` (`Genes_Nombre` ASC) VISIBLE,
-  INDEX `fk_Enfermedad_has_Genes_Enfermedad1_idx` (`Enfermedad_idEnfermedad` ASC) VISIBLE,
-  CONSTRAINT `fk_Enfermedad_has_Genes_Enfermedad1`
-    FOREIGN KEY (`Enfermedad_idEnfermedad`)
-    REFERENCES `TrabajoBDB`.`Enfermedad` (`idEnfermedad`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Enfermedad_has_Genes_Genes1`
-    FOREIGN KEY (`Genes_Nombre`)
-    REFERENCES `TrabajoBDB`.`Genes` (`Nombre`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `TrabajoBDB`.`Enfermedad` (`idEnfermedad`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
 -- Table `TrabajoBDB`.`Autor`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `TrabajoBDB`.`Autor` ;
+
 CREATE TABLE IF NOT EXISTS `TrabajoBDB`.`Autor` (
   `idAutor` INT NOT NULL,
-  `Nombre` VARCHAR(45) NULL,
+  `Nombre` VARCHAR(45) NULL DEFAULT NULL,
   PRIMARY KEY (`idAutor`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
 -- Table `TrabajoBDB`.`Articulo_has_Autor`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `TrabajoBDB`.`Articulo_has_Autor` ;
+
 CREATE TABLE IF NOT EXISTS `TrabajoBDB`.`Articulo_has_Autor` (
   `Articulo_idArticulo` VARCHAR(45) NOT NULL,
   `Autor_idAutor` INT NOT NULL,
@@ -162,15 +77,115 @@ CREATE TABLE IF NOT EXISTS `TrabajoBDB`.`Articulo_has_Autor` (
   INDEX `fk_Articulo_has_Autor_Articulo1_idx` (`Articulo_idArticulo` ASC) VISIBLE,
   CONSTRAINT `fk_Articulo_has_Autor_Articulo1`
     FOREIGN KEY (`Articulo_idArticulo`)
-    REFERENCES `TrabajoBDB`.`Articulo` (`idArticulo`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+    REFERENCES `TrabajoBDB`.`Articulo` (`idArticulo`),
   CONSTRAINT `fk_Articulo_has_Autor_Autor1`
     FOREIGN KEY (`Autor_idAutor`)
-    REFERENCES `TrabajoBDB`.`Autor` (`idAutor`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `TrabajoBDB`.`Autor` (`idAutor`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `TrabajoBDB`.`Genes`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `TrabajoBDB`.`Genes` ;
+
+CREATE TABLE IF NOT EXISTS `TrabajoBDB`.`Genes` (
+  `Nombre` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`Nombre`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `TrabajoBDB`.`Enfermedad_has_Genes`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `TrabajoBDB`.`Enfermedad_has_Genes` ;
+
+CREATE TABLE IF NOT EXISTS `TrabajoBDB`.`Enfermedad_has_Genes` (
+  `Enfermedad_idEnfermedad` VARCHAR(100) NOT NULL,
+  `Genes_Nombre` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`Enfermedad_idEnfermedad`, `Genes_Nombre`),
+  INDEX `fk_Enfermedad_has_Genes_Genes1_idx` (`Genes_Nombre` ASC) VISIBLE,
+  INDEX `fk_Enfermedad_has_Genes_Enfermedad1_idx` (`Enfermedad_idEnfermedad` ASC) VISIBLE,
+  CONSTRAINT `fk_Enfermedad_has_Genes_Enfermedad1`
+    FOREIGN KEY (`Enfermedad_idEnfermedad`)
+    REFERENCES `TrabajoBDB`.`Enfermedad` (`idEnfermedad`),
+  CONSTRAINT `fk_Enfermedad_has_Genes_Genes1`
+    FOREIGN KEY (`Genes_Nombre`)
+    REFERENCES `TrabajoBDB`.`Genes` (`Nombre`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `TrabajoBDB`.`Proteina`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `TrabajoBDB`.`Proteina` ;
+
+CREATE TABLE IF NOT EXISTS `TrabajoBDB`.`Proteina` (
+  `idProteina` VARCHAR(45) NOT NULL,
+  `Longitud` INT NULL DEFAULT NULL,
+  `Nombre` VARCHAR(200) NULL DEFAULT NULL,
+  `Organismo` VARCHAR(100) NULL DEFAULT NULL,
+  PRIMARY KEY (`idProteina`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `TrabajoBDB`.`Proteina_has_Enfermedad`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `TrabajoBDB`.`Proteina_has_Enfermedad` ;
+
+CREATE TABLE IF NOT EXISTS `TrabajoBDB`.`Proteina_has_Enfermedad` (
+  `Proteina_idProteina` VARCHAR(45) NOT NULL,
+  `Enfermedad_idEnfermedad` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`Proteina_idProteina`, `Enfermedad_idEnfermedad`),
+  INDEX `fk_Proteina_has_Enfermedad_Enfermedad1_idx` (`Enfermedad_idEnfermedad` ASC) VISIBLE,
+  INDEX `fk_Proteina_has_Enfermedad_Proteina1_idx` (`Proteina_idProteina` ASC) VISIBLE,
+  CONSTRAINT `fk_Proteina_has_Enfermedad_Enfermedad1`
+    FOREIGN KEY (`Enfermedad_idEnfermedad`)
+    REFERENCES `TrabajoBDB`.`Enfermedad` (`idEnfermedad`),
+  CONSTRAINT `fk_Proteina_has_Enfermedad_Proteina1`
+    FOREIGN KEY (`Proteina_idProteina`)
+    REFERENCES `TrabajoBDB`.`Proteina` (`idProteina`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `TrabajoBDB`.`Sintomas`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `TrabajoBDB`.`Sintomas` ;
+
+CREATE TABLE IF NOT EXISTS `TrabajoBDB`.`Sintomas` (
+  `idSintomas` VARCHAR(45) NOT NULL,
+  `Descripcion` VARCHAR(100) NULL DEFAULT NULL,
+  PRIMARY KEY (`idSintomas`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `TrabajoBDB`.`Sintomas_has_Enfermedad`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `TrabajoBDB`.`Sintomas_has_Enfermedad` ;
+
+CREATE TABLE IF NOT EXISTS `TrabajoBDB`.`Sintomas_has_Enfermedad` (
+  `Sintomas_idSintomas` VARCHAR(45) NOT NULL,
+  `Enfermedad_idEnfermedad` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`Sintomas_idSintomas`, `Enfermedad_idEnfermedad`),
+  INDEX `fk_Sintomas_has_Enfermedad_Enfermedad1_idx` (`Enfermedad_idEnfermedad` ASC) VISIBLE,
+  INDEX `fk_Sintomas_has_Enfermedad_Sintomas1_idx` (`Sintomas_idSintomas` ASC) VISIBLE,
+  CONSTRAINT `fk_Sintomas_has_Enfermedad_Enfermedad1`
+    FOREIGN KEY (`Enfermedad_idEnfermedad`)
+    REFERENCES `TrabajoBDB`.`Enfermedad` (`idEnfermedad`),
+  CONSTRAINT `fk_Sintomas_has_Enfermedad_Sintomas1`
+    FOREIGN KEY (`Sintomas_idSintomas`)
+    REFERENCES `TrabajoBDB`.`Sintomas` (`idSintomas`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
---
+++ inserciones/ProcesadoCSV.py
@@ -0,0 +1,83 @@
+import random
+
+def cargarProteinas(nomFichero, inserciones):
+    proteinas = open(nomFichero, 'r')
+    try:
+        next(proteinas)
+
+        for lin in proteinas:
+            idP, nom, org, lon = lin.split(sep=';')
+            i = "INSERT INTO Proteina (idProteina, Longitud, Nombre, Organismo) VALUES ("
+            i = i + "'" + idP + "', " + lon.strip() + ", '" + nom + "', '" + org + "');"
+            inserciones.append(i)
+        
+    finally:
+        proteinas.close()
+        
+def cargarEnfermedades(nomFichero, inserciones, idP):
+    enfermedades = open(nomFichero, 'r')
+    try:
+        for lin in enfermedades:
+            if (lin[0].isdigit() == True):
+                inser = "INSERT INTO Enfermedad (idEnfermedad, CambioProteina, SignificanciaClinica) VALUES ("
+                nombre = lin[9:len(lin)-1]
+                lin = next(enfermedades)
+                genes = lin[9:len(lin)-1]
+                lin = next(enfermedades)
+                cambio = lin[16:len(lin)-1]
+                if (cambio == "\n"):
+                    cambio = "Not registered"
+                lin = next(enfermedades)
+                sintomas = lin[14:len(lin)-1]
+                lin = next(enfermedades)
+                significance = lin[23:len(lin)-1]
+                
+                inser = inser + "'" + nombre + "', '" + cambio + "', '" + significance + "');"
+                inserciones.append(inser)
+                inserProtEnfe = "INSERT INTO Proteina_has_Enfermedad (Proteina_idProteina, Enfermedad_idEnfermedad) VALUES ('" + idP + "', '" + nombre + "');"
+                inserciones.append(inserProtEnfe)
+                cargarGenes(nombre, genes, inserciones)
+                cargarSintomas(nombre, sintomas, inserciones)
+                
+    finally:
+        enfermedades.close()
+        
+def cargarGenes(idEnfermedad, genes, inserciones):
+    arrayGen = genes.split(",")
+        
+    for i in range(len(arrayGen)):
+        gen = arrayGen[i].strip()
+        inserGen = "INSERT IGNORE INTO Genes (Nombre) VALUES ('" + gen + "');"
+        inserciones.append(inserGen)
+        inserEnferGen = "INSERT INTO Enfermedad_has_Genes (Enfermedad_idEnfermedad, Genes_Nombre) VALUES ('" + idEnfermedad + "', '" + gen + "');"
+        inserciones.append(inserEnferGen)
+        
+def cargarSintomas(idEnfermedad, sintomas, inserciones):
+    arraySintomas = sintomas.split(",")
+    
+    for i in range(len(arraySintomas)):
+        sintoma = arraySintomas[i].strip()
+        idSin = random.randint(1, 10000)
+        inserSintoma = "INSERT INTO Sintomas (idSintomas, Descripcion) VALUES ('" + str(idSin) + "', '" + sintoma + "');"
+        inserciones.append(inserSintoma)
+        inserEnferSintoma = "INSERT INTO Sintomas_has_Enfermedad (Sintomas_idSintomas, Enfermedad_idEnfermedad) VALUES (" + str(idSin) + ", '" + idEnfermedad + "');"
+        inserciones.append(inserEnferSintoma)
+    
+def importarInserciones(nomFichero, inserciones):
+    inser = open(nomFichero, 'w')
+    try:
+        for i in range(0,len(inserciones)):
+            a = inserciones[i]
+            inser.write(a)
+            inser.write("\n")
+    finally:
+        inser.close()
+
+# ----------------------------------
+
+inser = []
+idProteinas = ["P14735", "P06213", "A2VEY9"] 
+cargarProteinas("proteinas.csv", inser)
+for i in range(len(idProteinas)):
+    cargarEnfermedades(idProteinas[i] + ".txt", inser, idProteinas[i])
+importarInserciones("inserciones.sql", inser)
